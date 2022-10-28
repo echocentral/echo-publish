9ce87a4152bdd8d68a48dc173c82c7d1a2f5049d
echocentral <andrewcaelliott+ec@gmail.com>
2022-10-28 15:17:52 +0100

    build/first_unit_tests test files in fixtures

--- Makefile
+++ Makefile
@@ -4,4 +4,5 @@ VARIABLE_SECRETS_DIR ?= '~/mocha/secrets'
 test:
 	./epvenv39/Scripts/python -m pytest tests
 
-
+test_only:
+	./epvenv39/Scripts/python -m pytest tests/$(case).py -k $(k)
--- tests/test_django_engine.py
+++ tests/test_django_engine.py
@@ -1,6 +1,8 @@
-from unittest import TestCase
 import os
-from merge.utils.engine_utils import get_engine, substituteVariablesPlainString
+from parameterized import parameterized
+from unittest import TestCase
+import filecmp
+from merge.utils.engine_utils import get_engine, substituteVariablesPlainString, substituteVariablesPlain
 from merge.models import ClientConfig
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "merge.engine_settings")
@@ -9,14 +11,14 @@ application = get_wsgi_application()
 
 class TestDjangoEngine(TestCase):
     
-    def test_get_engine(self):
+    def _test_get_engine(self):
         config = ClientConfig()
         config.tenant = ('.')
         engine = get_engine(config)
         print(engine)
         self.assertTrue(True)
 
-    def test_render_string(self):
+    def _test_render_string(self):
         config = ClientConfig()
         config.tenant = ('.')
         engine = get_engine(config)
@@ -26,3 +28,33 @@ class TestDjangoEngine(TestCase):
         stringOut = substituteVariablesPlainString(config, stringIn, subs)
         expected = "This is version 1a written in 2022"
         self.assertEqual(stringOut, expected)
+
+    def _test_render_string_2(self):
+        config = ClientConfig()
+        config.tenant = ('.')
+        engine = get_engine(config)
+        print(engine)
+        subs = {"version": "1a", "year": 2022}
+        stringIn = "This is version {{ version }} written in {{ year }}"
+        stringOut = substituteVariablesPlainString(config, stringIn, subs)
+        expected = "This is version 1a written in 2022"
+        self.assertEqual(stringOut, expected)
+
+    @parameterized.expand([
+        ('./tests/fixtures/SampleText01.txt', 'a',
+            {'adj1': 'trivial', 'adj2': 'useful', "company": {"name": "ABC Inc."}}), 
+        ('./tests/fixtures/SampleText01.txt', 'b',
+            {'adj1': 'trivial', 'adj2': 'useful', "company": {"name": "XYZ Inc."}}), 
+        ('./tests/fixtures/SampleText02.txt', 'a',
+            {"versions": ["1a", "1b"], "company": {"name": "XYZ Inc."}}) 
+    ])    
+    def test_render_file(self, fixture, variant, subs):
+        config = ClientConfig()
+        config.tenant = ('.')
+        filenameIn = fixture
+        filenameOut = filenameIn.replace('fixtures', 'results').replace('.txt', f'.{variant}.txt')
+        filenameExpected = filenameOut.replace('results', 'expected')
+        fileOut = substituteVariablesPlain(config, filenameIn, filenameOut, subs)
+        self.assertEqual(fileOut['file'], filenameOut)
+        self.assertTrue(filecmp.cmp(filenameOut, filenameExpected))
+
